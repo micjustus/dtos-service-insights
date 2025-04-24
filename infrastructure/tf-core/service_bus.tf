5157d07617541e04a316bb04a1a79f2e11341a85
Alastair Lock <alastair.lock@gmail.com>
2025-04-24 14:15:59 +0100

    DTOSS-8565: Create a Terraform module for Azure Service Bus

--- infrastructure/tf-core/service_bus.tf
+++ infrastructure/tf-core/service_bus.tf
@@ -3,8 +3,8 @@ module "azure_service_bus" {
 
   source = "./modules/service-bus"
 
-  servicebus_topic_name     = "servicebus_topic"
-  servicebus_namespace_name = "servicebus_namespace"
+  servicebus_topic_name     = "servicebus-topic"
+  servicebus_namespace_name = "servicebus-namespace"
   resource_group_name       = azurerm_resource_group.core[each.key].name
   location                  = each.key
   capacity                  = 1
