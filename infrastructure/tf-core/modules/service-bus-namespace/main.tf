c38f03a8168d02f2520b636ae2abbed04f6ef15b
Alastair Lock <alastair.lock@gmail.com>
2025-04-24 13:30:39 +0100

    DTOSS-8565: Create a Terraform module for Azure Service Bus

--- infrastructure/tf-core/modules/service-bus-namespace/main.tf
+++ infrastructure/tf-core/modules/service-bus-namespace/main.tf
@@ -1,9 +1,16 @@
 resource "azurerm_servicebus_namespace" "this" {
-  name                = var.service_bus_namespace_name
+  name                = var.servicebus_namespace_name
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku_tier
   capacity            = var.capacity
 
   tags = var.tags
+}
+
+resource "azurerm_servicebus_topic" "this" {
+  name         = var.servicebus_topic_name
+  namespace_id = azurerm_servicebus_namespace.this.id
+
+  partitioning_enabled = true
 }
--- infrastructure/tf-core/modules/service-bus-namespace/variables.tf
+++ infrastructure/tf-core/modules/service-bus-namespace/variables.tf
@@ -1,6 +1,14 @@
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group in which to create the Event Grid. Changing this forces a new resource to be created."
+variable "capacity" {
+  description = "When sku is Premium, capacity can be 1, 2, 4, 8 or 16. When sku is Basic or Standard, capacity must be 0."
+  type        = number
+  default     = 1
+  validation {
+    condition = (
+      (var.sku_tier == "Premium" && contains([1, 2, 4, 8, 16], var.capacity)) ||
+      ((var.sku_tier == "Basic" || var.sku_tier == "Standard") && var.capacity == 0)
+    )
+    error_message = "Invalid capacity: Premium allows 1, 2, 4, 8 or 16. Basic and Standard must have capacity 0."
+  }
 }
 
 variable "location" {
@@ -13,9 +21,31 @@ variable "location" {
   }
 }
 
-variable "service_bus_namespace_name" {
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which to create the Event Grid. Changing this forces a new resource to be created."
+  validation {
+    condition     = can(regex("^[-\\w\\._\\(\\)]+$", var.resource_group_name)) && length(var.resource_group_name) > 0
+    error_message = "The resource group name must be a non-empty string using only alphanumeric characters, dashes, underscores, periods, or parentheses."
+  }
+}
+
+variable "servicebus_namespace_name" {
   description = "The name of the Service Bus namespace."
   type        = string
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9][a-zA-Z0-9-]{6,49}$", var.servicebus_namespace_name))
+    error_message = "The Service Bus namespace name must be between 7 and 50 characters and can contain only letters, numbers, and hyphens. It must start with a letter or number."
+  }
+}
+
+variable "servicebus_topic_name" {
+  description = "The name of the Service Bus topic."
+  type        = string
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9\\-\\.]{1,260}$", var.servicebus_topic_name))
+    error_message = "The topic name must be 1-260 characters long and may contain letters, numbers, hyphens, and periods."
+  }
 }
 
 variable "sku_tier" {
@@ -32,17 +62,4 @@ variable "tags" {
   description = "A mapping of tags to assign to the resource."
   type        = map(string)
   default     = {}
-}
-
-variable "capacity" {
-  description = "When sku is Premium, capacity can be 1, 2, 4, 8 or 16. When sku is Basic or Standard, capacity must be 0."
-  type        = number
-  default     = 1
-  validation {
-    condition = (
-      (var.sku_tier == "Premium" && contains([1, 2, 4, 8, 16], var.capacity)) ||
-      ((var.sku_tier == "Basic" || var.sku_tier == "Standard") && var.capacity == 0)
-    )
-    error_message = "Invalid capacity: Premium allows 1, 2, 4, 8 or 16. Basic and Standard must have capacity 0."
-  }
 }
